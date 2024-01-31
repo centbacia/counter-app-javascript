9eed6e8a74817d638ca08d1f481e2943c3077f3c
Vincent Bacia <vincentbacia947@example.com>
2024-01-31 15:29:38 +0800

    my first js js activity

---
+++ script.js
@@ -0,0 +1,29 @@
+const btnincrement=document.querySelector(".btn-increment");
+const btndecrement=document.querySelector(".btn-decrement");
+const number=document.querySelector("h2");
+const btnchange=document.querySelector(".bg-color")
+const body=document.querySelector("body")
+
+let counter=0;
+
+btnincrement.addEventListener('click',function(){
+counter++;
+number.textContent=counter;
+})
+
+btndecrement.addEventListener('click',function(){
+  counter--;
+  number.textContent=counter;
+})
+
+btnchange.addEventListener('click',function(){
+  number.style.backgroundColor="green";
+
+
+})
+
+btnchange.addEventListener('click',function(){
+    body.style.backgroundImage='url(123.jpg)';
+  
+  
+  })
